25fb7a10a7f06fde05f996594db7fcd6c421d325
Ivan Dimitrov <idimitrov@devexperts.com>
2023-05-06 14:35:08 +0300

    Add Application Configuration

--- src/main/java/com/ls/security/config/JwtAuthenticationFilter.java
+++ src/main/java/com/ls/security/config/JwtAuthenticationFilter.java
@@ -6,6 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.CachingUserDetailsService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,10 +19,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtService jwtService;
-
     private static final String HEADER_NAME = "Authorization";
     private static final String AUTHENTICATION_HEADER_PREFIX = "Bearer ";
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,
                                     @NonNull HttpServletResponse response,
@@ -30,5 +36,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         final String jwt = authHeader.substring(AUTHENTICATION_HEADER_PREFIX.length());
         final String userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && !isUserAuthenticated()) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
+    }
+
+    private boolean isUserAuthenticated() {
+        return SecurityContextHolder.getContext().getAuthentication() != null;
     }
 }
