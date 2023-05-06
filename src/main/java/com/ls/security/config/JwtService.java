ecb2458b0223633a99fec2912a548c9c36e2f65c
Ivan Dimitrov <idimitrov@devexperts.com>
2023-05-06 14:25:00 +0300

    Add token validation

--- src/main/java/com/ls/security/config/JwtService.java
+++ src/main/java/com/ls/security/config/JwtService.java
@@ -42,6 +42,19 @@ public class JwtService {
                 .compact();
     }
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return !isTokenExpired(token) && username.equals(userDetails.getUsername());
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
